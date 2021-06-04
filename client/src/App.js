2ea50f27359cefc55c0c6af84d4423ea14093485
Amul <amulagrawal32@gmail.com>
2021-06-04 14:52:51 +0530

    Adding basics skeleton for post and form and navBar
    Signed-off-by: Amul <amulagrawal32@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
-
+import memories from "./images/memories.png";
+import "./AppStyle.css";
+import Form from "./Componets/Form/Form";
+import Posts from "./Componets/Posts/Posts";
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="main-container">
+      <div className="AppBar">
+        <h2>Memories</h2>
+        <img className="mem-logo" src={memories} alt="Memories"></img>
+      </div>
+      <div className="container">
+        <div>
+          <Posts />
+        </div>
+        <div>
+          <Form />
+        </div>
+      </div>
     </div>
   );
 }
